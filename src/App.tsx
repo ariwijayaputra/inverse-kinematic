e33cbca8aab45ab8839d6ce28433c9964dea7acb
ariwijayaputra <aryanathaa@gmail.com>
2024-05-12 00:38:12 +0800

    add z chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,244 +1,236 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import "./App.css";
 import inverseKinematics from "./lib/inverseKinematic";
 import { toast } from "react-toastify";
-import forwardKinematics from "./lib/forwardKinematic";
 import ChartKinematic from "./components/chartKinematic";
 
 function App() {
-    const [joints, setJoints] = useState<
-        { x: number; y: number }[] | undefined
-    >();
-    const [alpha, setAlpha] = useState<number>(0);
-    const [beta, setBeta] = useState<number>(0);
-    const [gamma, setGamma] = useState<number>(0);
-    const [X, setX] = useState<number>(0);
-    const [Y, setY] = useState<number>(0);
-    const [Z, setZ] = useState<number>(0);
-    const [L1, setL1] = useState<number>(0);
-    const [L2, setL2] = useState<number>(0);
+	const [joints, setJoints] = useState<
+		{ x: number; y: number; z: number }[] | undefined
+	>();
+	const [alpha, setAlpha] = useState<number>(0);
+	const [beta, setBeta] = useState<number>(0);
+	const [gamma, setGamma] = useState<number>(0);
+	const [X, setX] = useState<number>(0);
+	const [Y, setY] = useState<number>(0);
+	const [Z, setZ] = useState<number>(0);
+	const [L1, setL1] = useState<number>(0);
+	const [L2, setL2] = useState<number>(0);
 
-    const handleCalculate = (e: FormEvent) => {
-        e.preventDefault();
-        console.log(L1, L2, X, Y);
-        try {
-            const [a, b, c, newJoint] = inverseKinematics(X, Y, Z, L1, L2);
-            console.log(a, b, c, newJoint);
-            // console.log(forwardKinematics(a, b, L1, L2));
-            setAlpha(a);
-            setBeta(b);
-            setGamma(c);
-            setJoints(newJoint);
-        } catch (error: any) {
-            toast(error.message as string, { theme: "dark", type: "error" });
-        }
-    };
+	const handleCalculate = (e?: FormEvent) => {
+		e?.preventDefault();
+		console.log(L1, L2, X, Y);
+		try {
+			if (L1 && L2) {
+				const [a, b, c, newJoint] = inverseKinematics(X, Y, Z, L1, L2);
+				console.log(a, b, c, newJoint);
+				// console.log(forwardKinematics(a, b, L1, L2));
+				setAlpha(a);
+				setBeta(b);
+				setGamma(c);
+				setJoints(newJoint);
+			}
+		} catch (error: any) {
+			toast(error.message as string, { theme: "dark", type: "error" });
+		}
+	};
+	// useEffect(() => {
+	// 	handleCalculate();
+	// }, [X, Y, Z, L1, L2]);
+	return (
+		<>
+			<div style={{ display: "flex", flexDirection: "row" }}>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						width: "30%",
+					}}>
+					<h1 style={{ textAlign: "start" }}>Inverse Kinematic Simulator</h1>
+					<form
+						onSubmit={(e) => handleCalculate(e)}
+						style={{
+							display: "flex",
+							gap: "20px",
+							flexDirection: "column",
+						}}>
+						<div
+							style={{
+								display: "flex",
+								width: "100%",
+								flexDirection: "column",
+								gap: "8px",
+							}}>
+							<div
+								style={{
+									display: "flex",
+									width: "100%",
+									flexDirection: "row",
+									gap: "8px",
+								}}>
+								<span style={{ width: "28px" }}>L1</span>
+								<input
+									value={L1}
+									onChange={(e) => setL1(parseInt(e.target.value))}
+									style={{ width: "100%" }}
+									type="number"
+									name="l1"></input>
+							</div>
+							<div
+								style={{
+									display: "flex",
+									width: "100%",
+									flexDirection: "row",
+									gap: "8px",
+								}}>
+								<span style={{ width: "28px" }}>L2</span>
+								<input
+									value={L2}
+									onChange={(e) => setL2(parseInt(e.target.value))}
+									style={{ width: "100%" }}
+									type="number"
+									name="l2"></input>
+							</div>
+						</div>
+						<div
+							style={{
+								display: "flex",
+								width: "100%",
+								flexDirection: "column",
+								gap: "8px",
+							}}>
+							<div
+								style={{
+									display: "flex",
+									width: "100%",
+									flexDirection: "row",
+									gap: "8px",
+								}}>
+								<span style={{ width: "28px" }}>X</span>
+								<input
+									value={X}
+									onChange={(e) => setX(parseInt(e.target.value))}
+									style={{ width: "100%" }}
+									type="number"
+									name="x"></input>
+							</div>
+							<div
+								style={{
+									display: "flex",
+									width: "100%",
+									flexDirection: "row",
+									gap: "8px",
+								}}>
+								<span style={{ width: "28px" }}>Y</span>
+								<input
+									value={Y}
+									onChange={(e) => setY(parseInt(e.target.value))}
+									style={{ width: "100%" }}
+									type="number"
+									name="y"></input>
+							</div>
+							<div
+								style={{
+									display: "flex",
+									width: "100%",
+									flexDirection: "row",
+									gap: "8px",
+								}}>
+								<span style={{ width: "28px" }}>Z</span>
+								<input
+									value={Z}
+									onChange={(e) => setZ(parseInt(e.target.value))}
+									style={{ width: "100%" }}
+									type="number"
+									name="z"></input>
+							</div>
+						</div>
+						<button type="submit">Calculate</button>
+					</form>
 
-    return (
-        <>
-            <div style={{ display: "flex", flexDirection: "row" }}>
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        width: "30%",
-                    }}>
-                    <h1 style={{ textAlign: "start" }}>
-                        Inverse Kinematic Simulator
-                    </h1>
-                    <form
-                        onSubmit={(e) => handleCalculate(e)}
-                        style={{
-                            display: "flex",
-                            gap: "20px",
-                            flexDirection: "column",
-                        }}>
-                        <div
-                            style={{
-                                display: "flex",
-                                width: "100%",
-                                flexDirection: "column",
-                                gap: "8px",
-                            }}>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    width: "100%",
-                                    flexDirection: "row",
-                                    gap: "8px",
-                                }}>
-                                <span style={{ width: "28px" }}>L1</span>
-                                <input
-                                    value={L1}
-                                    onChange={(e) =>
-                                        setL1(parseInt(e.target.value))
-                                    }
-                                    style={{ width: "100%" }}
-                                    type="number"
-                                    name="l1"></input>
-                            </div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    width: "100%",
-                                    flexDirection: "row",
-                                    gap: "8px",
-                                }}>
-                                <span style={{ width: "28px" }}>L2</span>
-                                <input
-                                    value={L2}
-                                    onChange={(e) =>
-                                        setL2(parseInt(e.target.value))
-                                    }
-                                    style={{ width: "100%" }}
-                                    type="number"
-                                    name="l2"></input>
-                            </div>
-                        </div>
-                        <div
-                            style={{
-                                display: "flex",
-                                width: "100%",
-                                flexDirection: "column",
-                                gap: "8px",
-                            }}>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    width: "100%",
-                                    flexDirection: "row",
-                                    gap: "8px",
-                                }}>
-                                <span style={{ width: "28px" }}>X</span>
-                                <input
-                                    value={X}
-                                    onChange={(e) =>
-                                        setX(parseInt(e.target.value))
-                                    }
-                                    style={{ width: "100%" }}
-                                    type="number"
-                                    name="x"></input>
-                            </div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    width: "100%",
-                                    flexDirection: "row",
-                                    gap: "8px",
-                                }}>
-                                <span style={{ width: "28px" }}>Y</span>
-                                <input
-                                    value={Y}
-                                    onChange={(e) =>
-                                        setY(parseInt(e.target.value))
-                                    }
-                                    style={{ width: "100%" }}
-                                    type="number"
-                                    name="y"></input>
-                            </div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    width: "100%",
-                                    flexDirection: "row",
-                                    gap: "8px",
-                                }}>
-                                <span style={{ width: "28px" }}>Z</span>
-                                <input
-                                    value={Z}
-                                    onChange={(e) =>
-                                        setZ(parseInt(e.target.value))
-                                    }
-                                    style={{ width: "100%" }}
-                                    type="number"
-                                    name="z"></input>
-                            </div>
-                        </div>
-                        <button type="submit">Calculate</button>
-                    </form>
-
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "8px",
-                        }}>
-                        <div
-                            style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                gap: "8px",
-                            }}>
-                            <div
-                                style={{
-                                    background: "#fdd",
-                                    padding: "4px",
-                                }}></div>
-                            <p>degree 1: {alpha}</p>
-                        </div>
-                        <div
-                            style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                gap: "8px",
-                            }}>
-                            <div
-                                style={{
-                                    background: "green",
-                                    padding: "4px",
-                                }}></div>
-                            <p>degree 2: {beta}</p>
-                        </div>
-                        <div
-                            style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                gap: "8px",
-                            }}>
-                            <div
-                                style={{
-                                    background: "blue",
-                                    padding: "4px",
-                                }}></div>
-                            <p>degree 3: {gamma}</p>
-                        </div>
-                    </div>
-                </div>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                    <div
-                        className="card"
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            width: "400px",
-                            height: "400px",
-                        }}>
-                        {joints && (
-                            <ChartKinematic joints={joints} range={L1 + L2} />
-                        )}
-                    </div>
-                    <div
-                        className="card"
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            width: "400px",
-                            height: "400px",
-                        }}>
-                        {joints && (
-                            <ChartKinematic
-                                joints={[
-                                    { x: 0, y: 0 },
-                                    { y: Z, x: joints[2].x },
-                                ]}
-                                range={L1 + L2}
-                            />
-                        )}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							gap: "8px",
+						}}>
+						<div
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								gap: "8px",
+							}}>
+							<div
+								style={{
+									background: "#fdd",
+									padding: "4px",
+								}}></div>
+							<p>degree 1: {alpha}</p>
+						</div>
+						<div
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								gap: "8px",
+							}}>
+							<div
+								style={{
+									background: "green",
+									padding: "4px",
+								}}></div>
+							<p>degree 2: {beta}</p>
+						</div>
+						<div
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								gap: "8px",
+							}}>
+							<div
+								style={{
+									background: "blue",
+									padding: "4px",
+								}}></div>
+							<p>degree 3: {gamma}</p>
+						</div>
+					</div>
+				</div>
+				<div style={{ display: "flex", flexDirection: "row" }}>
+					<div
+						className="card"
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							width: "400px",
+							height: "400px",
+						}}>
+						{"x -> and y^"}
+						{joints && <ChartKinematic joints={joints} range={L1 + L2} />}
+					</div>
+					<div
+						className="card"
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							width: "400px",
+							height: "400px",
+						}}>
+						{"x -> and z^ (from top)"}
+						{joints && (
+							<ChartKinematic
+								joints={[
+									{ x: 0, y: 0 },
+									{ x: joints[1].x, y: joints[1].z },
+									{ y: Z, x: joints[2].x },
+								]}
+								range={L1 + L2}
+							/>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/lib/inverseKinematic.ts
+++ src/lib/inverseKinematic.ts
@@ -8,58 +8,53 @@
  * @returns An array containing the joint angles [theta1, theta2] in radians.
  */
 export default function inverseKinematics(
-    x: number,
-    y: number,
-    z: number,
-    l1: number,
-    l2: number
-): [number, number, number, { x: number; y: number }[]] {
-    // let invertMode = false;
-    // if (x > 0) {
-    // 	x = x * -1;
-    // 	invertMode = true;
-    // }
-    const c2 = (x ** 2 + y ** 2 - l1 ** 2 - l2 ** 2) / (2 * l1 * l2);
+	x: number,
+	y: number,
+	z: number,
+	l1: number,
+	l2: number
+): [number, number, number, { x: number; y: number; z: number }[]] {
+	// find the needed leg length to reach (x,z)
+	const legLength = Math.sqrt(x ** 2 + z ** 2);
+	// find how much degree the leg needed to rotate to reach x,z
+	const diagonal = Math.sqrt(legLength ** 2 + z ** 2);
+	const radians = Math.acos(z / diagonal);
+	const theta0Deg = radians * (180 / Math.PI) - 90;
 
-    // Calculate the base rotation angle (theta0)
-    const legLength = Math.sqrt(x ** 2 + z ** 2)
-	
-    console.log("legLength", legLength);
-    const diagonal = Math.sqrt(legLength ** 2 + z ** 2);
-    const radians = Math.acos(z / diagonal);
-    const theta0Deg = radians * (180 / Math.PI) - 90;
+	x = legLength * -1;
+	const c2 = (x ** 2 + y ** 2 - l1 ** 2 - l2 ** 2) / (2 * l1 * l2);
 
-    // Check if the desired position is within the reachable workspace
-    if (c2 > 1 || c2 < -1) {
-        throw new Error("The desired position is not reachable.");
-    }
+	// Check if the desired position is within the reachable workspace
+	if (c2 > 1 || c2 < -1) {
+		throw new Error("The desired position is not reachable.");
+	}
 
-    const s2 = Math.sqrt(1 - c2 ** 2);
-    const theta2Rad = Math.atan2(s2, c2);
-    let theta2Deg = (theta2Rad * 180) / Math.PI;
+	const s2 = Math.sqrt(1 - c2 ** 2);
+	const theta2Rad = Math.atan2(s2, c2);
+	let theta2Deg = (theta2Rad * 180) / Math.PI;
 
-    const k1 = l1 + l2 * Math.cos(theta2Rad);
-    const k2 = l2 * Math.sin(theta2Rad);
-    const theta1Rad = Math.atan2(y, x) - Math.atan2(k2, k1);
-    let theta1Deg = (theta1Rad * 180) / Math.PI;
+	const k1 = l1 + l2 * Math.cos(theta2Rad);
+	const k2 = l2 * Math.sin(theta2Rad);
+	const theta1Rad = Math.atan2(y, x) - Math.atan2(k2, k1);
+	let theta1Deg = (theta1Rad * 180) / Math.PI;
 
-    // if x > 0 invert degree
-    // if (invertMode === true) {
-    // 	theta1Deg = 180 - theta1Deg;
-    // 	theta2Deg = 180 - theta2Deg;
-    // }
+	// Calculate the positions of each joint
+	const joint1Position: { x: number; y: number; z: 0 } = { x: 0, y: 0, z: 0 };
+	const joint2X = l1 * Math.cos(theta1Rad);
+	const joint2Y = l1 * Math.sin(theta1Rad);
+	const joint2Z = joint2X * Math.tan((theta0Deg * Math.PI) / 180);
+	console.log(joint2X, "joint2x", theta0Deg, "theta0Deg");
+	const joint2Position: { x: number; y: number; z: number } = {
+		x: joint2X,
+		y: joint2Y,
+		z: joint2Z,
+	};
+	const endEffectorPosition: { x: number; y: number; z: number } = { x: x, y: y, z: z };
 
-    // Calculate the positions of each joint
-    const joint1Position: { x: number; y: number } = { x: 0, y: 0 };
-    const joint2X = l1 * Math.cos(theta1Rad);
-    const joint2Y = l1 * Math.sin(theta1Rad);
-    const joint2Position: { x: number; y: number } = { x: joint2X, y: joint2Y };
-    const endEffectorPosition: { x: number; y: number } = { x: x, y: y };
-
-    return [
-        theta1Deg,
-        theta2Deg,
-        theta0Deg,
-        [joint1Position, joint2Position, endEffectorPosition],
-    ];
+	return [
+		theta1Deg,
+		theta2Deg,
+		theta0Deg,
+		[joint1Position, joint2Position, endEffectorPosition],
+	];
 }
